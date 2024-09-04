4a02e8cefc6b26d967c3341854c445704c7be7c2
Tommy Hedley <tommyhedley@gmail.com>
2024-09-03 23:54:25 -0700

    Restructured and added oauth2 handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/tommyhedley/fibery/tsheets
+module github.com/tommyhedley/fiberytsheets
 
-go 1.22.4
+go 1.23.0
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/tommyhedley/fiberytsheets/internal/handlers"
+)
 
 func main() {
-	fmt.Println("hello world")
+	godotenv.Load()
+	config := handlers.AppConfig{
+		ID:          "qbtime",
+		Name:        "Quickbooks Time",
+		Version:     "0.1.0",
+		Description: "Integrate Quickbooks Time with Fibery",
+		Authentication: []struct {
+			ID          string   `json:"id"`
+			Name        string   `json:"name"`
+			Description string   `json:"description"`
+			Fields      []string `json:"fields"`
+		}{
+			{
+				ID:          "public",
+				Name:        "Public Access",
+				Description: "There is no authentication required",
+				Fields:      []string{},
+			},
+		},
+		Sources: []string{},
+		ResponsibleFor: struct {
+			DataSynchronization bool `json:"dataSynchronization"`
+		}{
+			DataSynchronization: true,
+		},
+	}
+
+	port := os.Getenv("PORT")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/healthz", handlers.Readiness)
+	mux.HandleFunc("GET /", config.Config)
+	mux.HandleFunc("POST /oauth2/v1/authorize", handlers.Authorize)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Server started on port: %s", port)
+	log.Fatal(srv.ListenAndServe())
 }
